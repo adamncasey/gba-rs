b4cbb377fb532287caef95c02ac90bd4930efeb5
Adam Casey <adamncasey@gmail.com>
2021-09-19 23:49:56 +0100

    More decoding (not working)

--- gbars/src/instruction.rs
+++ gbars/src/instruction.rs
@@ -1,4 +1,5 @@
 use crate::cpu::Register;
+use bitvec::prelude::*;
 
 #[derive(Debug, PartialEq, Eq)]
 pub struct Instruction {
@@ -101,26 +102,73 @@ fn read_condition(cond: u32) -> Condition {
 }
 
 fn read_instruction_op(op: u32) -> InstructionOp {
-    let b_27_26 = op >> 26;
+    let bits = op.view_bits::<Lsb0>();
+    let b_27_26 = &bits[26..=27];
 
     match b_27_26 {
-        0b00 => {
-            // look at bit 7..=4
-            // Multiply, Swap, halfword transfer, BX, Data Processing
+        bits![0, 0] => {
+            if bits[25] {
+                // decode Data Processing with Immediate operand2
+                return decode_data_processing(true, &bits[0..=24])
+            }
+            else {
+                if bits[4] {
+                    if bits[7] {
+                        if bits[5] || bits[6] {
+                            if bits[22] {
+                                // Halfword transfer immediate offset
+                            }
+                            else {
+                                // Halfword transfer register offset
+                            }
+                        }
+                        else {
+                            if bits[24] {
+                                // Swap
+                            }
+                            else {
+                                if bits[23] {
+                                    // Multiply Long
+                                }
+                                else {
+                                    // Multiply
+                                }
+                            }
+                        }
+                        // Multiply, Swap, halfword transfer
+                    }
+                    else {
+                        // BX
+                    }
+                }
+                else {
+                    // Data Processing with Register operand2
+                }
+            }
         },
-        0b01 => {
-            // Single Data Transfer / Undefined
+        bits![0, 1] => {
+            if bits[25] && bits[4] {
+                // Undefined instruction
+            }
+            else {
+                // Single Data Transfer
+            }
         },
-        0b10 => {
-            // if b25 branch
-            // else block data transfer
+        bits![1, 0] => {
+            if bits[25] {
+                // Branch
+            }
+            else {
+                // Block data transfer
+            }
         },
-        0b11 => {
+        bits![1, 1] => {
             // Coprocessor / Software interrupt
         },
         _ => unimplemented!("bad read_instruction_op input: {} {}", op, b_27_26)
     }
 
+    
     InstructionOp::DataProcessing {
         opcode: DataProcessingOpCode::And,
         dest: Register::R5,
@@ -130,6 +178,46 @@ fn read_instruction_op(op: u32) -> InstructionOp {
             value: 3,
         },
         alter_condition: false,
+    }
+}
+
+fn decode_data_processing(immediate: bool, bits: &BitSlice<Lsb0, u32>) -> InstructionOp {
+
+    let alter_condition = bits[20];
+    let opcode = read_dataprocessing_opcode(&bits[21..=24]);
+
+    InstructionOp::DataProcessing {
+        opcode,
+        dest: Register::R5,
+        operand1: Register::R5,
+        operand2: Operand::Immediate {
+            rotate: 0,
+            value: 3,
+        },
+        alter_condition,
+    }
+}
+
+fn read_dataprocessing_opcode(bits: &BitSlice<Lsb0, u32>) -> DataProcessingOpCode
+{
+    match bits {
+        bits![0,0,0,0] => DataProcessingOpCode::And,
+        bits![0,0,0,1] => DataProcessingOpCode::Eor,
+        bits![0,0,1,0] => DataProcessingOpCode::Sub,
+        bits![0,0,1,1] => DataProcessingOpCode::Rsb,
+        bits![0,1,0,0] => DataProcessingOpCode::Add,
+        bits![0,1,0,1] => DataProcessingOpCode::Adc,
+        bits![0,1,1,0] => DataProcessingOpCode::Sbc,
+        bits![0,1,1,1] => DataProcessingOpCode::Rsc,
+        bits![1,0,0,0] => DataProcessingOpCode::Tst,
+        bits![1,0,0,1] => DataProcessingOpCode::Teq,
+        bits![1,0,1,0] => DataProcessingOpCode::Cmp,
+        bits![1,0,1,1] => DataProcessingOpCode::Cmn,
+        bits![1,1,0,0] => DataProcessingOpCode::Orr,
+        bits![1,1,0,1] => DataProcessingOpCode::Mov,
+        bits![1,1,1,0] => DataProcessingOpCode::Bic,
+        bitvec![Lsb0, u32; 1,1,1,1] => DataProcessingOpCode::Mvn,
+        _ => panic!("Bad input to read_dataprocessing_opecode: {:?} ", bits.to_bitvec()),
     }
 }
 
