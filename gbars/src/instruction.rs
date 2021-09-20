cf1ff13d0ab4a9ec01cd3172b5f46714232018e7
Adam Casey <adamncasey@gmail.com>
2021-09-20 01:10:24 +0100

    Working add/sub/mov decode. Failing store/push decode

--- gbars/src/instruction.rs
+++ gbars/src/instruction.rs
@@ -103,121 +103,146 @@ fn read_condition(cond: u32) -> Condition {
 
 fn read_instruction_op(op: u32) -> InstructionOp {
     let bits = op.view_bits::<Lsb0>();
-    let b_27_26 = &bits[26..=27];
+    let b_27_26 = op >> 26;
 
     match b_27_26 {
-        bits![0, 0] => {
+        0b00 => {
             if bits[25] {
                 // decode Data Processing with Immediate operand2
-                return decode_data_processing(true, &bits[0..=24])
+                return decode_data_processing(true, op);
             }
             else {
                 if bits[4] {
                     if bits[7] {
                         if bits[5] || bits[6] {
                             if bits[22] {
-                                // Halfword transfer immediate offset
+                                unimplemented!("Halfword transfer immediate offset");
                             }
                             else {
-                                // Halfword transfer register offset
+                                unimplemented!("Halfword transfer register offset");
                             }
                         }
                         else {
                             if bits[24] {
-                                // Swap
+                                unimplemented!("Swap");
                             }
                             else {
                                 if bits[23] {
-                                    // Multiply Long
+                                    unimplemented!("Multiply Long");
                                 }
                                 else {
-                                    // Multiply
+                                    unimplemented!("Multiply");
                                 }
                             }
                         }
-                        // Multiply, Swap, halfword transfer
+                        unimplemented!("Multiply, Swap, halfword transfer");
                     }
                     else {
-                        // BX
+                        unimplemented!("BX");
                     }
                 }
                 else {
-                    // Data Processing with Register operand2
+                    return decode_data_processing(false, op);
                 }
             }
         },
-        bits![0, 1] => {
+        0b01 => {
             if bits[25] && bits[4] {
-                // Undefined instruction
+                unimplemented!("Undefined instruction");
             }
             else {
-                // Single Data Transfer
+                unimplemented!("Single Data Transfer");
             }
         },
-        bits![1, 0] => {
+        0b10 => {
             if bits[25] {
-                // Branch
+                unimplemented!("Branch");
             }
             else {
-                // Block data transfer
+                unimplemented!("Block data transfer");
             }
         },
-        bits![1, 1] => {
-            // Coprocessor / Software interrupt
+        0b11 => {
+            unimplemented!("Coprocessor / Software interrupt");
         },
         _ => unimplemented!("bad read_instruction_op input: {} {}", op, b_27_26)
     }
-
-    
-    InstructionOp::DataProcessing {
-        opcode: DataProcessingOpCode::And,
-        dest: Register::R5,
-        operand1: Register::R5,
-        operand2: Operand::Immediate {
-            rotate: 0,
-            value: 3,
-        },
-        alter_condition: false,
-    }
 }
 
-fn decode_data_processing(immediate: bool, bits: &BitSlice<Lsb0, u32>) -> InstructionOp {
+fn decode_data_processing(immediate: bool, bits: u32) -> InstructionOp {
+    let alter_condition = ((bits >> 20) & 0b1) != 0;
+    let opcode = read_dataprocessing_opcode(((bits >> 21) & 0b1111) as u8);
 
-    let alter_condition = bits[20];
-    let opcode = read_dataprocessing_opcode(&bits[21..=24]);
+    let rd = read_register(((bits >> 12) & 0b1111) as u8);
+    let rn = read_register(((bits >> 16) & 0b1111) as u8);
+
+    let operand2 = if immediate {
+        Operand::Immediate {
+            rotate: ((bits >> 8) & 0b1111) as u8,
+            value: (bits & 0b11111111) as u8,
+        }
+    } else {
+        let rm = read_register((bits & 0b1111) as u8);
+
+        let shift = ((bits >> 4) & 0b11111111) as u8;
+
+        Operand::Register {
+            shift,
+            register: rm
+        }
+    };
 
     InstructionOp::DataProcessing {
         opcode,
-        dest: Register::R5,
-        operand1: Register::R5,
-        operand2: Operand::Immediate {
-            rotate: 0,
-            value: 3,
-        },
+        dest: rd,
+        operand1: rn,
+        operand2,
         alter_condition,
     }
 }
 
-fn read_dataprocessing_opcode(bits: &BitSlice<Lsb0, u32>) -> DataProcessingOpCode
+fn read_dataprocessing_opcode(bits: u8) -> DataProcessingOpCode
 {
     match bits {
-        bits![0,0,0,0] => DataProcessingOpCode::And,
-        bits![0,0,0,1] => DataProcessingOpCode::Eor,
-        bits![0,0,1,0] => DataProcessingOpCode::Sub,
-        bits![0,0,1,1] => DataProcessingOpCode::Rsb,
-        bits![0,1,0,0] => DataProcessingOpCode::Add,
-        bits![0,1,0,1] => DataProcessingOpCode::Adc,
-        bits![0,1,1,0] => DataProcessingOpCode::Sbc,
-        bits![0,1,1,1] => DataProcessingOpCode::Rsc,
-        bits![1,0,0,0] => DataProcessingOpCode::Tst,
-        bits![1,0,0,1] => DataProcessingOpCode::Teq,
-        bits![1,0,1,0] => DataProcessingOpCode::Cmp,
-        bits![1,0,1,1] => DataProcessingOpCode::Cmn,
-        bits![1,1,0,0] => DataProcessingOpCode::Orr,
-        bits![1,1,0,1] => DataProcessingOpCode::Mov,
-        bits![1,1,1,0] => DataProcessingOpCode::Bic,
-        bitvec![Lsb0, u32; 1,1,1,1] => DataProcessingOpCode::Mvn,
-        _ => panic!("Bad input to read_dataprocessing_opecode: {:?} ", bits.to_bitvec()),
+        0b0000 => DataProcessingOpCode::And,
+        0b0001 => DataProcessingOpCode::Eor,
+        0b0010 => DataProcessingOpCode::Sub,
+        0b0011 => DataProcessingOpCode::Rsb,
+        0b0100 => DataProcessingOpCode::Add,
+        0b0101 => DataProcessingOpCode::Adc,
+        0b0110 => DataProcessingOpCode::Sbc,
+        0b0111 => DataProcessingOpCode::Rsc,
+        0b1000 => DataProcessingOpCode::Tst,
+        0b1001 => DataProcessingOpCode::Teq,
+        0b1010 => DataProcessingOpCode::Cmp,
+        0b1011 => DataProcessingOpCode::Cmn,
+        0b1100 => DataProcessingOpCode::Orr,
+        0b1101 => DataProcessingOpCode::Mov,
+        0b1110 => DataProcessingOpCode::Bic,
+        0b1111 => DataProcessingOpCode::Mvn,
+        _ => panic!("Bad input to read_dataprocessing_opecode: {:?} ", bits),
+    }
+}
+
+fn read_register(bits: u8) -> Register {
+    match bits {
+        0b0000 => Register::R0,
+        0b0001 => Register::R1,
+        0b0010 => Register::R2,
+        0b0011 => Register::R3,
+        0b0100 => Register::R4,
+        0b0101 => Register::R5,
+        /*0b0110 => Register::R6,
+        0b0111 => Register::R7,
+        0b1000 => Register::R8,
+        0b1001 => Register::R9,
+        0b1010 => Register::R10,
+        0b1011 => Register::R11,
+        0b1100 => Register::R12,*/
+        0b1101 => Register::R13,
+        0b1110 => Register::R14,
+        0b1111 => Register::R15,
+        _ => panic!("Bad input to read_register: {:?} ", bits),
     }
 }
 
@@ -240,6 +265,75 @@ fn test_add_decode() {
             operand2: Operand::Register {
                 shift: 0,
                 register: Register::R2,
+            },
+            alter_condition: false,
+        },
+    };
+
+    assert_eq!(instr, expected);
+}
+
+#[test]
+fn test_sub_decode() {
+    let op = 0xe24dd014;
+
+    let instr = Instruction::decode_arm(op);
+
+    let expected = Instruction {
+        cond: Condition::Always,
+        instr: InstructionOp::DataProcessing {
+            opcode: DataProcessingOpCode::Sub,
+            dest: Register::R13,
+            operand1: Register::R13,
+            operand2: Operand::Immediate {
+                rotate: 0,
+                value: 20,
+            },
+            alter_condition: false,
+        },
+    };
+
+    assert_eq!(instr, expected);
+}
+
+#[test]
+fn test_mov_decode() {
+    let op = 0xe3a03005;
+
+    let instr = Instruction::decode_arm(op);
+
+    let expected = Instruction {
+        cond: Condition::Always,
+        instr: InstructionOp::DataProcessing {
+            opcode: DataProcessingOpCode::Mov,
+            dest: Register::R3,
+            operand1: Register::R0,
+            operand2: Operand::Immediate {
+                rotate: 0,
+                value: 5,
+            },
+            alter_condition: false,
+        },
+    };
+
+    assert_eq!(instr, expected);
+}
+
+#[test]
+fn test_push_decode() {
+    let op = 0xe52db004;
+
+    let instr = Instruction::decode_arm(op);
+
+    let expected = Instruction {
+        cond: Condition::Always,
+        instr: InstructionOp::DataProcessing {
+            opcode: DataProcessingOpCode::Mov,
+            dest: Register::R3,
+            operand1: Register::R0,
+            operand2: Operand::Immediate {
+                rotate: 0,
+                value: 5,
             },
             alter_condition: false,
         },
--- gbars/tests/test-roms/README.md
+++ gbars/tests/test-roms/README.md
@@ -1,4 +1,5 @@
 
 ```
 arm-none-eabi-gcc build/crt0.s test1.c -mcpu=arm7tdmi -nostartfiles -Tbuild/lnkscript -o test1.gba
+arm-none-eabi-gcc add.c -mcpu=arm7tdmi -nostartfiles -Tbuild/lnkscript -o add.gba
 ```
